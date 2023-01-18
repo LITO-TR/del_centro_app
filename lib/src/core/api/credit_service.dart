5f51efdb827e5075d05199c5e7e9c4e7d4e5343d
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-18 12:33:00 -0500

    fix url of api and added validations

--- lib/src/core/api/credit_service.dart
+++ lib/src/core/api/credit_service.dart
@@ -2,27 +2,28 @@ import 'dart:convert';
 
 import 'package:del_centro_app/src/core/models/credit.dart';
 import 'package:http/http.dart' as http;
-class CreditService{
 
-  // final String url = "https://10.0.2.2:5000/api/credit/";
+class CreditService {
+  final String url = "http://localhost:9000/api/credit";
 
-  void createCredit(double creditAmount, double decimalInterest, int numberOfPayments, String paymentMethod, double mora) async{
+  Future<Credit> createCredit(double creditAmount, double decimalInterest,
+      int numberOfPayments, String paymentMethod, double mora) async {
     final credit = {
       "creditAmount": creditAmount,
-    "decimalInterest": decimalInterest,
-    "numberOfPayments": numberOfPayments,
-    "paymentMethod": paymentMethod,
-    "mora": mora
+      "decimalInterest": decimalInterest,
+      "numberOfPayments": numberOfPayments,
+      "paymentMethod": paymentMethod,
+      "mora": mora
     };
     final headers = {"Content-Type": "application/json;charset=UTF-8"};
-    final res = await http.post(Uri.parse("http://localhost:9000/api/credit"),
-        headers: headers, body: jsonEncode(credit));
-    print('exito');
-    print(res.body);
+    final res = await http.post(Uri.parse(url),
+        headers: headers, body: jsonEncode(credit)); // credit a json
 
-  // var fintest = jsonDecode(response.body);
-  //return fintest;
+    if (res.statusCode == 200) {
+      var obj = Credit.fromJson(jsonDecode(res.body)); //json a credit
 
-}
-
+      return obj;
+    } else
+      throw Exception('Failed to load post');
+  }
 }
