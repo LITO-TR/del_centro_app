30f6d6890c6557607d4e4ba58ad7baf2b59dcd4c
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-19 16:46:31 -0500

    added card for credit created

--- lib/src/features/credit/credit_page.dart
+++ lib/src/features/credit/credit_page.dart
@@ -1,12 +1,11 @@
-import 'dart:convert';
 
 import 'package:del_centro_app/src/core/api/credit_service.dart';
 import 'package:del_centro_app/src/core/models/credit.dart';
+import 'package:del_centro_app/src/features/credit/widgets/customer_options.dart';
 import 'package:del_centro_app/src/features/credit/widgets/input_credit.dart';
 import 'package:del_centro_app/src/features/shared/widgets/button_with_icon.dart';
-import 'package:del_centro_app/styles/styles.dart';
+import 'package:del_centro_app/src/styles/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class CreditPage extends StatefulWidget {
   const CreditPage({Key? key}) : super(key: key);
@@ -16,7 +15,7 @@ class CreditPage extends StatefulWidget {
 }
 
 class _CreditPageState extends State<CreditPage> {
-  static const List<String> list = <String>['DIARIO', 'SEMANAL', 'QUINCENAL'];
+  static const List<String> list = <String>['day', 'week', 'QUINCENAL'];
   final txtCreditAmount = TextEditingController();
   final txtInterest = TextEditingController();
   final txtNumberOfPayments = TextEditingController();
@@ -73,14 +72,24 @@ class _CreditPageState extends State<CreditPage> {
           width: MediaQuery.of(context).size.width / 2.5,
           height: MediaQuery.of(context).size.height / 1.05,
           decoration: BoxDecoration(
-              color: Styles.backgroundContainer,
-              borderRadius: BorderRadius.circular(5)),
+              color: Styles.white, borderRadius: BorderRadius.circular(10)),
           child: Column(
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
-                  children: const [Text('DATOS DEL CREDITO')],
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.monetization_on,
+                      color: Colors.green,
+                    ),
+                    Text(
+                      'DATOS CREDITO',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )
+                  ],
                 ),
               ),
               Row(
@@ -108,31 +117,58 @@ class _CreditPageState extends State<CreditPage> {
                           width: 120),
                     ],
                   ),
-                  Container(
-                    width: 150,
-                    height: 75,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[300],
-                        //border: Border.all(color: Colors.grey, width: 1),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Total: S/$totalAmount'),
-                        Text('Cuota: S/$payments')
-                      ],
-                    ),
+                  Column(
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(Icons.table_view,color: Colors.green,),
+                          Text('SIMULADOR', style: TextStyle(fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+
+                      Container(
+                        width: 150,
+                        height: 75,
+                        decoration: BoxDecoration(
+                            color: Styles.blueDark,
+                            //border: Border.all(color: Colors.grey, width: 1),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Total: S/$totalAmount',
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              'Cuota: S/$payments',
+                              style: const TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
-                    Text('DATOS DEL CLIENTE'),
+                    Icon(
+                      Icons.person,
+                      color: Colors.green,
+                    ),
+                    Text(
+                      'DATOS CLIENTE',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
                   ],
                 ),
               ),
+             // CustomerOptions(), process
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
@@ -172,12 +208,14 @@ class _CreditPageState extends State<CreditPage> {
               ),
               Container(
                 decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColorLight,
+                  color: Styles.scaffoldBackgroundColor,
                   borderRadius: BorderRadius.circular(5),
                 ),
                 width: 150,
                 child: DropdownButtonFormField(
                     value: dropDownValue,
+                    dropdownColor: Styles.backgroundOrange,
+                    iconEnabledColor: Styles.blueDark,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(borderSide: BorderSide.none),
                     ),
@@ -217,22 +255,216 @@ class _CreditPageState extends State<CreditPage> {
           width: MediaQuery.of(context).size.width / 2.5,
           height: MediaQuery.of(context).size.height / 1.05,
           decoration: BoxDecoration(
-              color: Styles.backgroundContainer,
-              borderRadius: BorderRadius.circular(5)),
+              color: Styles.white, borderRadius: BorderRadius.circular(10)),
           child: FutureBuilder(
             future: creditCreated,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
+                var credit = snapshot.data!;
                 return Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
                       width: 400,
-                      height: 525,
+                
                       decoration: BoxDecoration(
-                          color: Styles.backgroundMainColor,
+                          color: Styles.blueDark,
                           borderRadius: BorderRadius.circular(10)),
-                      child: Text(snapshot.data!.decimalInterest.toString()),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 300,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Styles.scaffoldBackgroundColor,
+                              ),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Icon(
+                                    Icons.person_outline,
+                                    size: 40,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Juan Junior Paredes Rojas',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text('72781103'),
+                                      Text('Jr. Pachacutecx N°204'),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'S/ ' + credit.creditAmount.toString(),
+                                    style: const TextStyle(
+                                        fontSize: 25, color: Colors.white),
+                                  ),
+                                  Text(
+                                    'Monto del Credito',
+                                    style: TextStyle(
+                                        color: Styles.backgroundOrange),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'S/. ' + credit.paymentsAmount.toString(),
+                                    style: const TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white
+                                    ),
+                                  ),
+                                  Text(
+                                    'Couta ',
+                                    style:
+                                    TextStyle(color: Styles.backgroundOrange),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    (credit.decimalInterest! * 100).toString() +
+                                        ' %',
+                                    style: const TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white
+                                    ),
+                                  ),
+                                  Text(
+                                    'Interes',
+                                    style: TextStyle(
+                                        color: Styles.backgroundOrange),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'S/ ${credit.totalAmount}',
+                                    style: const TextStyle(
+                                        fontSize: 25, color: Colors.white),
+                                  ),
+                                  Text(
+                                    'Devolucion Total',
+                                    style: TextStyle(
+                                        color: Styles.backgroundOrange),
+                                  )
+                                ],
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    credit.numberOfPayments.toString(),
+                                    style: const TextStyle(
+                                        fontSize: 25,
+                                        color: Colors.white
+                                    ),
+                                  ),
+                                  Text(
+                                    'Numero de Cuotas ',
+                                    style:
+                                    TextStyle(color: Styles.backgroundOrange),
+                                  ),
+                                ],
+                              ),
+
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Styles.backgroundOrange,
+                              borderRadius: const BorderRadius.vertical(bottom: Radius.circular(10))
+
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Primer dia de Pago:  ',
+                                        style:
+                                        TextStyle(color: Styles.blueDark,
+                                        ),
+                                      ),
+                                      Text(
+                                        credit.firstPayDate.toString(),
+                                        style:  TextStyle(color: Styles.blueDark,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Ultimo dia de Pago:  ',
+                                        style:
+                                        TextStyle(color: Styles.blueDark,
+
+                                        ),
+                                      ),
+                                      Text(
+                                        credit.expirationDate.toString(),
+                                        style: TextStyle(color: Styles.blueDark,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                          ),
+
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ButtonWithIcon(
+                          color: Colors.green,
+                          iconData: Icons.print,
+                          onPressed: () {},
+                          label: 'CONTRATO'),
                     )
                   ],
                 );
@@ -240,7 +472,7 @@ class _CreditPageState extends State<CreditPage> {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
-                  Text('Creando un nuevo credito'),
+                  Text('Creando credito'),
                   CircularProgressIndicator()
                 ],
               );
