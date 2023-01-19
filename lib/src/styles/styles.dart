4b0f59ea8cfa7bf908681dd40764cce47507eb35
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-19 16:49:12 -0500

    added styles

---
+++ lib/src/styles/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static Color scaffoldBackgroundColor = const Color(0xFFefefef) ;
+  static Color blueDark = const  Color(0xFF283135);
+  static Color backgroundOrange = const  Color(0xFFed8c44);
+  static Color white = const Color(0XFFFFFFFF);
+}
