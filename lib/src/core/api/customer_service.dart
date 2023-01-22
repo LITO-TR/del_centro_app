fbfb576a024099a34de14318afd8c9a7f38211c2
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-22 16:33:02 -0500

    customer service

---
+++ lib/src/core/api/customer_service.dart
@@ -0,0 +1,20 @@
+import 'package:del_centro_app/src/core/models/credit.dart';
+import 'package:del_centro_app/src/core/models/customer.dart';
+import 'package:http/http.dart' as http;
+class CustomerService{
+
+  String url = "http://localhost:9000/api/customers";
+  Future<List<Customer>> getAllCustomers() async{
+  final res = await http.get(Uri.parse(url));
+
+    return customerFromJson(res.body);
+    //validators
+    //styles for cards
+    //search customer
+  }
+  
+  Future<List<Credit>> getCreditsByCustomer(String customerId) async{
+    final res = await http.get(Uri.parse('$url/$customerId/credits'));
+    return creditFromJson(res.body);
+  }
+}
