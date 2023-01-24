8925618bcfabc8166d98ef4366441b2d948f6f54
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-24 15:57:23 -0500

    added container for payments by date

--- lib/src/features/dashboard/dashboard_page.dart
+++ lib/src/features/dashboard/dashboard_page.dart
@@ -1,3 +1,5 @@
+import 'package:del_centro_app/src/core/api/credit_service.dart';
+import 'package:del_centro_app/src/core/models/payment.dart';
 import 'package:flutter/material.dart';
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -8,13 +10,38 @@ class DashboardPage extends StatefulWidget {
 
 class _DashboardPage extends State<DashboardPage> {
 
+  late Future<List<Payment>> payments;
+  final creditService = CreditService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    //payments = creditService
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [Text('hola')],
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width*0.9,
+            height: MediaQuery.of(context).size.height/2,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.red
+            ),
+            child: FutureBuilder<List<Payment>>(
+              future: payments,
+              builder: (context, snapshot){
+                return const CircularProgressIndicator();
+              },
+            )
+          )
+        ],
+      ),
     );
-
-
 
   }
 }
