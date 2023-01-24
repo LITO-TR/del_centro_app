77da6322ecd23d8cca71a3bbb4ac45ed6762c6ee
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-23 19:21:09 -0500

    added customer credits page

--- lib/src/features/customers/widgets/customer_credits.dart
+++ lib/src/features/customers/widgets/customer_credits.dart
@@ -1,38 +1,112 @@
+import 'package:del_centro_app/src/core/api/credit_service.dart';
+import 'package:del_centro_app/src/core/api/customer_service.dart';
 import 'package:del_centro_app/src/core/models/credit.dart';
+import 'package:del_centro_app/src/core/models/customer.dart';
+import 'package:del_centro_app/src/features/customers/widgets/customer_credit_detail.dart';
+import 'package:del_centro_app/src/styles/styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomerCredits extends StatefulWidget {
-  const CustomerCredits({Key? key,required this.credits}) : super(key: key);
-  final List<Credit> credits;
+  const CustomerCredits({Key? key, required this.customer}) : super(key: key);
+  final Customer customer;
   @override
   State<CustomerCredits> createState() => _CustomerCreditsState();
-
 }
-
-
 
 class _CustomerCreditsState extends State<CustomerCredits> {
-@override
-  void dispose() {
-  // TODO: implement dispose
-    super.dispose();
-    // widget.credits.clear();
-}
+  late Future<List<Credit>> credits;
+  final customerService = CustomerService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    credits = customerService.getCreditsByCustomer(widget.customer.id);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.credits.toString()),
-        centerTitle: true,
-      ),
-      body: Row(
-        children: [
-          const VerticalDivider(thickness: 1, width: 1),
-          // This is the main content.
-          // Expanded(child: _widgetOptions.elementAt(_selectedIndex)),
-          Text('customer detail')
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.customer.name),
+          centerTitle: true,
+          backgroundColor: Styles.blueDark,
+        ),
+        body: Column(
+          children: [
+            FutureBuilder<List<Credit>>(
+                future: credits,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+
+                    return Container(
+                      decoration: const BoxDecoration(),
+                      width: MediaQuery.of(context).size.width / 1.1,
+                      height: MediaQuery.of(context).size.height / 1.13,
+                      child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 4,
+                                  crossAxisSpacing: 4.0,
+                                  mainAxisSpacing: 4.0,
+                                  childAspectRatio: 1.8),
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            var credit = snapshot.data![index];
+                            Color colorStatusCredit = Colors.red;
+                            if(credit.creditStatus == "en proceso"){
+                               colorStatusCredit = Styles.backgroundOrange;
+                            }
+                            else if(credit.creditStatus == "finalizado"){
+                              colorStatusCredit = Colors.green;
+                            }
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context)=> CustomerCreditDetail(credit: credit,)));
+                              },
+                              child: Card(
+                                elevation: 4,
+                                color: Styles.white,
+                                shape: RoundedRectangleBorder(
+                                    side: const BorderSide(
+                                        color: Colors.transparent),
+                                    borderRadius: BorderRadius.circular(20)),
+                                child:    Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(),
+                                            child: Column(
+                                              children: [
+                                                Text(credit.creditAmount.toString(),style: TextStyle(fontSize: 25),),
+                                                Text('Monto de credito', style: TextStyle(color: Styles.backgroundOrange),)
+                                              ],
+                                            ),
+                                      ),
+                                      Text('${credit.firstPayDate.toString()} -  ${credit.expirationDate.toString()}'),
+                                      Container(
+                                        width: MediaQuery.of(context).size.width,
+                                        height: 30,
+                                        decoration: BoxDecoration(
+
+                                         color: colorStatusCredit,
+                                          borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
+                                        ),
+                                        child: Center(child: Text(credit.creditStatus.toString())),
+                                      )
+                                    ],
+                                  ),
+
+                              ),
+                            );
+                          }),
+                    );
+                  }
+                  return CircularProgressIndicator();
+                })
+          ],
+        ));
   }
 }
