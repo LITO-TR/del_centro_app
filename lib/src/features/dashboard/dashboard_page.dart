fadb8bb7156c1ebee7cef0c354d75b592cd6d9e9
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-24 19:34:00 -0500

    completed dashboard page

--- lib/src/features/dashboard/dashboard_page.dart
+++ lib/src/features/dashboard/dashboard_page.dart
@@ -1,6 +1,10 @@
 import 'package:del_centro_app/src/core/api/credit_service.dart';
+import 'package:del_centro_app/src/core/api/payment_service.dart';
+import 'package:del_centro_app/src/core/models/customer.dart';
 import 'package:del_centro_app/src/core/models/payment.dart';
+import 'package:del_centro_app/src/styles/styles.dart';
 import 'package:flutter/material.dart';
+
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
@@ -9,15 +13,22 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPage extends State<DashboardPage> {
+  late Future<List<Payment>> _payments;
+  late Future<Customer> _customer;
 
-  late Future<List<Payment>> payments;
+  final paymentService = PaymentService();
   final creditService = CreditService();
   @override
   void initState() {
     // TODO: implement initState
-    //payments = creditService
+    final date = DateTime.now();
+    _payments = paymentService.getPaymentsByDate(
+        date.day.toString(), date.month.toString(), date.year.toString());
+
     super.initState();
   }
+
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -26,22 +37,307 @@ class _DashboardPage extends State<DashboardPage> {
       child: Column(
         children: [
           Container(
-            width: MediaQuery.of(context).size.width*0.9,
-            height: MediaQuery.of(context).size.height/2,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.red
-            ),
-            child: FutureBuilder<List<Payment>>(
-              future: payments,
-              builder: (context, snapshot){
-                return const CircularProgressIndicator();
-              },
-            )
-          )
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Styles.blueDark,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '123',
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            ),
+                            Text(
+                              'Monto cobrado',
+                              style: TextStyle(
+                                  color: Styles.backgroundOrange,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Styles.blueDark,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '123',
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            ),
+                            Text(
+                              'Monto a cobrar',
+                              style: TextStyle(
+                                  color: Styles.backgroundOrange,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 100,
+                        decoration: BoxDecoration(
+                            color: Styles.blueDark,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '123',
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            ),
+                            Text(
+                              'Monto desembolsado',
+                              style: TextStyle(
+                                  color: Styles.backgroundOrange,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Text(
+                    'CUOTAS DE HOY -> ${DateTime.now().year}/${DateTime.now().month}/${DateTime.now().day}',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              )),
+          Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.6,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(13),
+              ),
+              child: FutureBuilder<List<Payment>>(
+                future: _payments,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+
+                    if (snapshot.data!.isEmpty) {
+                      return const Center(
+                        child: Text(
+                          'No hay cobranzas hoy',
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, int index) {
+                        Payment payment = snapshot.data![index];
+                        _customer = creditService.getCustomerByCreditId(payment.creditId.toString());
+                        Icon icon = Icon(Icons.error, color: Colors.red);
+                        if (payment.status == "PENDIENTE") {
+                          icon = Icon(
+                            Icons.check_circle,
+                            color: Colors.green,
+                          );
+                        } else if (payment.status == "PAGADO") {
+                          icon = Icon(
+                            Icons.error_outline,
+                            color: Colors.orange,
+                          );
+                        }
+                        return SizedBox(
+                          height: 70,
+                          child: Card(
+                              shape: RoundedRectangleBorder(
+                                  side: const BorderSide(
+                                      color: Colors.transparent),
+                                  borderRadius: BorderRadius.circular(20)),
+                              elevation: 4,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Nro',
+                                        style: TextStyle(
+                                            color: Styles.backgroundOrange),
+                                      ),
+                                      Text((index + 1).toString()),
+                                    ],
+                                  ),
+                            FutureBuilder<Customer>(
+                            future: _customer,
+                            builder:(context, snapshot){
+                              if(snapshot.hasData){
+                                Customer customer = snapshot.data!;
+                                return Row(
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.person,
+                                              color: Colors.yellow,
+                                            ),
+                                            Text(
+                                              'Cliente',
+                                              style: TextStyle(
+                                                  color: Styles.blueDark,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                        Text(customer.name),
+                                        Text(customer.lastName)
+                                      ],
+                                    ),
+
+                                    Column(
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.home,
+                                              color: Colors.brown,
+                                            ),
+                                            Text(
+                                              'Direccion',
+                                              style: TextStyle(
+                                                  color: Styles.blueDark,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                        Text(customer.address)
+                                      ],
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.phone,
+                                              color: Colors.green,
+                                            ),
+                                            Text(
+                                              'Cell',
+                                              style: TextStyle(
+                                                  color: Styles.blueDark,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                        Text(customer.phoneNumber)
+                                      ],
+                                    )
+
+                                  ],
+                                );
+                              }
+                              else if(snapshot.hasError){
+                                return Text(snapshot.error.toString());
+                              }
+                              return CircularProgressIndicator();
+                            }
+                          ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Pago:'),
+                                      Text(payment.payment.toString())
+                                    ],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [Text('Estado'), icon],
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Acciones'),
+                                      ElevatedButton(
+                                          onPressed: () {},
+                                          child: Icon(Icons.monetization_on))
+                                    ],
+                                  ),
+                                ],
+                              )
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    print('${snapshot.error}');
+                    return Text('error');
+                  }
+                  return Center(child: const CircularProgressIndicator());
+                },
+              )),
+          Column(
+            children: [
+              Row(children: [
+                Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                ),
+                Text(
+                  ' :PAGADO',
+                  style: TextStyle(
+                      color: Styles.blueDark, fontWeight: FontWeight.bold),
+                )
+              ]),
+              Row(children: [
+                Icon(
+                  Icons.error_outline,
+                  color: Colors.orange,
+                ),
+                Text(
+                  ' :PENDIENTE',
+                  style: TextStyle(
+                      color: Styles.blueDark, fontWeight: FontWeight.bold),
+                )
+              ])
+            ],
+          ),
         ],
       ),
     );
-
   }
 }
