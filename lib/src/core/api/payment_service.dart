a3e11bebee9e241c72eea6d5ba1fb94aa3a44fc0
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-25 21:15:15 -0500

    fixed set payment for api

--- lib/src/core/api/payment_service.dart
+++ lib/src/core/api/payment_service.dart
@@ -6,16 +6,25 @@ class PaymentService {
   String url = "http://localhost:9000/api/payments";
 
 
-  Future<List<Payment>> setPayment(String paymentId) async{
+  /*Future<List<Payment>> setPayment(String paymentId) async{
     final res = await http.put(Uri.parse('$url/$paymentId'));
     print(res.body);
     return paymentFromJson(res.body);
   }
-
+*/
   Future<List<Payment>> getPaymentsByDate(String day, String month, String year) async{
     print(day+month+year);
-    final res = await http.get(Uri.parse('$url/day/25/month/$month/year/$year'));
+    final res = await http.get(Uri.parse('$url/day/28/month/$month/year/$year'));
     return paymentFromJson(res.body);
 
   }
+
+  Future<Payment> setPayment(String paymentId) async{
+    final res = await http.put(Uri.parse('$url/$paymentId'));
+    print(res.body);
+    return paymentJson(res.body);
+  }
+
+
+
 }
