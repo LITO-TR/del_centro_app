17edc8c97ec355ff3990d838051ab37955e9b5b4
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-26 22:34:20 -0500

    added url for android

--- lib/src/core/api/customer_service.dart
+++ lib/src/core/api/customer_service.dart
@@ -4,6 +4,10 @@ import 'package:http/http.dart' as http;
 class CustomerService{
 
   String url = "http://localhost:9000/api/customers";
+
+  //String url = "http://10.0.2.2:9000/api/customers";
+
+
   Future<List<Customer>> getAllCustomers() async{
   final res = await http.get(Uri.parse(url));
 
