b61f34472069d98196551dc261d6146397aa132b
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-26 22:33:40 -0500

    erasing prints and added url for android

--- lib/src/core/api/credit_service.dart
+++ lib/src/core/api/credit_service.dart
@@ -7,8 +7,11 @@ import 'package:http/http.dart' as http;
 
 class CreditService {
    String url = "http://localhost:9000/api/credits";
+   //String url = "http://10.0.2.2:9000/api/credits";
 
-  Future<Credit> createCredit(double creditAmount, double decimalInterest,
+
+
+   Future<Credit> createCredit(double creditAmount, double decimalInterest,
       int numberOfPayments, String paymentMethod, double mora) async {
     final credit = {
       "creditAmount": creditAmount,
@@ -19,19 +22,16 @@ class CreditService {
       "customerId": '63c87bc4f110c0f21b2d0d59', // change id
       "employeeId": '63c87cb1f110c0f21b2d0d68' // change id
     };
+    print(decimalInterest);
     final headers = {"Content-Type": "application/json;charset=UTF-8"};
     final res = await http.post(Uri.parse(url),
         headers: headers, body: jsonEncode(credit)); // credit a json
     if (res.statusCode == 200) {
-      print('exit');
       var obj = Credit.fromJson(jsonDecode(res.body)); //json a credit
-
       return obj;
     } else if(res.statusCode == 400){
-      print('estoy aqui');
       throw Exception('Failed api');
     }else {
-      print('estoy aqui11');
       throw Exception('Failed to load');
     }
   }
