c160d871660ed515bfa562f713de4b29986e6faf
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-30 19:28:37 -0500

    changed customer class

--- lib/src/core/models/customer.dart
+++ lib/src/core/models/customer.dart
@@ -1,39 +1,35 @@
 import 'dart:convert';
 
-List<Customer> customerFromJson(String str) => List<Customer>.from(json.decode(str).map((x) => Customer.fromJson(x)));
-Customer customerJson(String str) => Customer.fromJson(jsonDecode(str));
+List<Customer> customersFromJson(String str) => List<Customer>.from(json.decode(str).map((x) => Customer.fromJson(x)));
+Customer customerFromJson(String str) => Customer.fromJson(jsonDecode(str));
 
-String customerToJson(List<Customer> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String customersToJson(List<Customer> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String customerToJson(Customer data)=> jsonEncode(data);
+
 
 class Customer {
   Customer({
-    required this.id,
-    required this.name,
-    required this.lastName,
-    required this.createdAt,
-    required this.updateAt,
-    required this.address,
-    required this.dni,
-    required this.civilStatus,
-    required this.phoneNumber
+     this.id,
+     this.name,
+     this.lastName,
+     this.address,
+     this.dni,
+     this.civilStatus,
+     this.phoneNumber
   });
 
-  String id;
-  String name;
-  String lastName;
-  DateTime createdAt;
-  DateTime updateAt;
-  String address;
-  String dni;
-  String civilStatus;
-  String phoneNumber;
+  String? id;
+  String? name;
+  String? lastName;
+  String? address;
+  String? dni;
+  String? civilStatus;
+  String? phoneNumber;
 
   factory Customer.fromJson(Map<String, dynamic> json) => Customer(
     id: json["_id"],
     name: json["name"],
     lastName: json["lastName"],
-    createdAt: DateTime.parse(json["createdAt"]),
-    updateAt: DateTime.parse(json["updateAt"]),
     address: json["address"],
     dni: json["DNI"],
     civilStatus: json["civilStatus"],
@@ -44,8 +40,6 @@ class Customer {
     "_id": id,
     "name": name,
     "lastName": lastName,
-    "createdAt": createdAt.toIso8601String(),
-    "updateAt": updateAt.toIso8601String(),
     "address": address,
     "DNI": dni,
     "civilStatus": civilStatus,
