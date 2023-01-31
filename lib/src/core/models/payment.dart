18e96891de2d399c2ccd5c1fb4f0f75ac06708b6
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-30 19:33:02 -0500

    added data for payment

--- lib/src/core/models/payment.dart
+++ lib/src/core/models/payment.dart
@@ -1,24 +1,25 @@
-
 import 'dart:convert';
 
-List<Payment> paymentFromJson(String str) => List<Payment>.from(json.decode(str).map((x) => Payment.fromJson(x)));
+List<Payment> paymentFromJson(String str) =>
+    List<Payment>.from(json.decode(str).map((x) => Payment.fromJson(x)));
 
-
-
-String paymentToJson(List<Payment> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String paymentToJson(List<Payment> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 Payment paymentJson(String str) => Payment.fromJson(jsonDecode(str));
 
 class Payment {
   Payment({
-     this.id,
-     this.paymentOrder,
-     this.date,
-     this.payment,
-     this.status,
-     this.paymentDate,
-     this.moraDays,
-     this.creditId,
+    this.id,
+    this.paymentOrder,
+    this.date,
+    this.payment,
+    this.status,
+    this.paymentDate,
+    this.moraDays,
+    this.paymentMethod,
+    this.customerPayment,
+    this.creditId,
   });
 
   String? id;
@@ -28,28 +29,33 @@ class Payment {
   String? status;
   String? paymentDate;
   int? moraDays;
+  String? paymentMethod;
+  double? customerPayment;
   String? creditId;
 
   factory Payment.fromJson(Map<String, dynamic> json) => Payment(
-    id: json["_id"],
-    paymentOrder: json["paymentOrder"],
-    date: json["date"],
-    payment: json["payment"].toDouble(),
-    status: json["status"],
-    paymentDate: json["paymentDate"],
-    moraDays: json["moraDays"],
-    creditId: json["creditId"],
-  );
+        id: json["_id"],
+        paymentOrder: json["paymentOrder"],
+        date: json["date"],
+        payment: json["payment"].toDouble(),
+        status: json["status"],
+        paymentDate: json["paymentDate"],
+        moraDays: json["moraDays"],
+        paymentMethod: json["paymentMethod"],
+        customerPayment: json["customerPayment"].toDouble(),
+        creditId: json["creditId"],
+      );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
-    "paymentOrder": paymentOrder,
-    "date": date,
-    "payment": payment,
-    "status": status,
-    "paymentDate": paymentDate,
-    "moraDays": moraDays,
-    "creditId":creditId,
-  };
+        "_id": id,
+        "paymentOrder": paymentOrder,
+        "date": date,
+        "payment": payment,
+        "status": status,
+        "paymentDate": paymentDate,
+        "moraDays": moraDays,
+        "paymentMethod": paymentMethod,
+        "customerPayment": customerPayment,
+        "creditId": creditId,
+      };
 }
-
