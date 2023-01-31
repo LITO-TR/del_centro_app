94ff6223ac0741e940fe741bf3d1fc57a032f20e
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-01-30 19:25:44 -0500

    added balance page

---
+++ lib/src/features/balance/balance_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class BalancePage extends StatefulWidget {
+  const BalancePage({Key? key}) : super(key: key);
+
+  @override
+  State<BalancePage> createState() => _BalancePageState();
+}
+
+class _BalancePageState extends State<BalancePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Balanceee'),);
+  }
+}
