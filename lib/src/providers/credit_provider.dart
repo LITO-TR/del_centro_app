3161af3d34b0dfa4b761b05147864f907031e90c
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-02-15 14:54:36 -0500

    credit provider implementation

--- lib/src/providers/credit_provider.dart
+++ lib/src/providers/credit_provider.dart
@@ -1,5 +1,27 @@
+import 'package:del_centro_app/src/core/services/credit_service.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../core/models/credit.dart';
+import '../core/models/customer.dart';
+
 class CreditProvider extends ChangeNotifier{
+  Credit _creditCreated = Credit();
+  bool isLoading = true;
+
+  Credit get creditCreated => _creditCreated;
+
+  void createCredit(
+      double creditAmount,
+      double decimalInterest,
+      int numberOfPayments,
+      String paymentMethod,
+      double mora,
+      Customer customer) async{
+    isLoading = false;
+    _creditCreated = await CreditService().createCredit(creditAmount, decimalInterest, numberOfPayments, paymentMethod, mora, customer);
+
+    notifyListeners();
+  }
+
 
 }
