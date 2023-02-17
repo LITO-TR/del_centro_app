ed0b7d9faed523ab33f08b05e11ea847dce979c8
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-02-17 10:58:06 -0500

    code refactor with provider

--- lib/src/features/customers/screen/customer_credits.dart
+++ lib/src/features/customers/screen/customer_credits.dart
@@ -1,10 +1,11 @@
-import 'package:del_centro_app/src/core/services/customer_service.dart';
-import 'package:del_centro_app/src/core/models/credit.dart';
+
 import 'package:del_centro_app/src/core/models/customer.dart';
 import 'package:del_centro_app/src/features/customers/screen/customer_credit_detail.dart';
 import 'package:del_centro_app/src/features/customers/widgets/credit_card.dart';
+import 'package:del_centro_app/src/providers/customer_provider.dart';
 import 'package:del_centro_app/src/styles/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CustomerCredits extends StatefulWidget {
   const CustomerCredits({Key? key, required this.customer}) : super(key: key);
@@ -14,17 +15,17 @@ class CustomerCredits extends StatefulWidget {
 }
 
 class _CustomerCreditsState extends State<CustomerCredits> {
-  late Future<List<Credit>> credits;
-  final customerService = CustomerService();
-  late List<Credit> creditData;
   @override
   void initState() {
     // TODO: implement initState
-    credits = customerService.getCreditsByCustomer(widget.customer.id.toString());
+    Provider.of<CustomerProvider>(context, listen: false)
+        .getCreditsByCustomerId(widget.customer.id.toString());
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
+    final customerProvider = context.watch<CustomerProvider>();
+
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.customer.name.toString()),
@@ -41,51 +42,55 @@ class _CustomerCreditsState extends State<CustomerCredits> {
                     width: MediaQuery.of(context).size.width * 0.9,
                     height: MediaQuery.of(context).size.height * 0.3,
                     decoration: BoxDecoration(
-                        border: Border.all(color: Styles.blueDark)
+                        border: Border.all(color: Styles.blueDark)),
+                    child: const Center(
+                      child: Text('Datos del cliente'),
                     ),
-                    child: const Center(child: Text('Datos del cliente'),),
                   ),
                 ),
-                FutureBuilder<List<Credit>>(
-                    future: credits,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        creditData = snapshot.data!;
-                        return Container(
-                          decoration: const BoxDecoration(),
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height *0.6,
-                          child: GridView.builder(
-                              gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 4,
-                                  crossAxisSpacing: 4.0,
-                                  mainAxisSpacing: 4.0,
-                                  childAspectRatio: 1.8),
-                              itemCount: snapshot.data!.length,
-                              itemBuilder: (context, index) {
-                                var credit = snapshot.data![index];
-                                Color colorStatusCredit = Colors.red;
-                                if(creditData[index].creditStatus == "en proceso"){
-                                  colorStatusCredit = Styles.backgroundOrange;
-                                }
-                                else if(creditData[index].creditStatus == "finalizado"){
-                                  colorStatusCredit = Colors.green;
-                                }
-                                return GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(context, MaterialPageRoute(builder: (context)=> CustomerCreditDetail(credit: credit,customer: widget.customer,)));
-                                  },
-                                  child: CreditCard(credit: credit,color: colorStatusCredit,)
-                                );
-                              }),
-                        );
-                      }
-                      return const CircularProgressIndicator();
-                    })
+               customerProvider.isLoading ?const Center(child: CircularProgressIndicator()): Container(
+                  decoration: const BoxDecoration(),
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4,
+                              crossAxisSpacing: 4.0,
+                              mainAxisSpacing: 4.0,
+                              childAspectRatio: 1.8),
+                      itemCount: customerProvider.listCreditsByCustomer.length,
+                      itemBuilder: (context, index) {
+                        var credit = customerProvider.listCreditsByCustomer[index];
+                        Color colorStatusCredit = Colors.red;
+                       if (customerProvider.listCreditsByCustomer[index].creditStatus == "en proceso") {
+                         colorStatusCredit = Styles.backgroundOrange;
+                        } else if (customerProvider.listCreditsByCustomer[index].creditStatus ==
+                            "finalizado") {
+                         colorStatusCredit = Colors.green;
+                       }
+                        return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          CustomerCreditDetail(
+                                            credit: customerProvider.listCreditsByCustomer[index],
+                                            customer: widget.customer,
+                                          )));
+                            },
+                            child: CreditCard(
+                              credit: credit,
+                              color: colorStatusCredit,
+                            ));
+                      }),
+                ),
               ],
             ),
           ],
         ));
   }
 }
+
+
