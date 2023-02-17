52a907c005d2ab7302bbb038154d65026d4014d0
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-02-17 10:59:25 -0500

    customer provider added

--- lib/src/providers/customer_provider.dart
+++ lib/src/providers/customer_provider.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../core/models/credit.dart';
 import '../core/models/customer.dart';
+import '../core/models/payment.dart';
+import '../core/services/credit_service.dart';
 import '../core/services/customer_service.dart';
-
+import '../core/services/payment_service.dart';
 class CustomerProvider extends ChangeNotifier {
   bool isLoading = false;
   List<Customer> _customerList = [];
-  List<Credit> _creditCustomerList = [];
+  List<Credit> _listCreditsByCustomer = [];
   List<Customer> get customerList => _customerList;
-  List<Credit> get creditCustomerList => _creditCustomerList;
-   Customer _customerSelected = Customer();
+  List<Credit> get listCreditsByCustomer => _listCreditsByCustomer;
+
+  Customer _customerSelected = Customer();
   void getCustomers() async{
     isLoading = true;
     _customerList = await CustomerService().getAllCustomers();
@@ -25,12 +28,12 @@ class CustomerProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void getCreditByCustomerId(String customerId) async{
-    if(_customerList.isEmpty){
-      _creditCustomerList = [];
-    }else{
-      _creditCustomerList = await CustomerService().getCreditsByCustomer(customerId);
-    }
+  void getCreditsByCustomerId(String customerId) async{
+  isLoading = true;
+    _listCreditsByCustomer = await CustomerService().getCreditsByCustomerId(customerId);
+  isLoading = false;
+notifyListeners();
+
   }
   void addCustomer(Customer customer) async{
     await CustomerService().registerCustomer(customer);
@@ -38,8 +41,35 @@ class CustomerProvider extends ChangeNotifier {
     notifyListeners();
 
   }
-  void deleteCustomer(String customerId)async {
-    await CustomerService().deteletCustomer(customerId);
+  List<Payment> _listPaymentsByCreditId = [];
+  Credit _creditSelected = Credit();
+  List<Payment> get listPaymentsByCreditId => _listPaymentsByCreditId;
+  Credit get creditSelected => _creditSelected;
+
+
+
+  void makePayment(String customerId, String creditId, int index,String paymentId, String method, double customerPayment)async{
+    isLoading = true;
+    _listPaymentsByCreditId[index] = await PaymentService().setPayment(paymentId, method, customerPayment);
+    _creditSelected = await CreditService().getCreditbyId(creditId);
+    _listCreditsByCustomer = await CustomerService().getCreditsByCustomerId(customerId);
+    isLoading = false;
     notifyListeners();
   }
+
+  void getPaymentsByCreditId(String creditId) async {
+    isLoading = true;
+    _listPaymentsByCreditId = await CreditService().getPaymentsByCredit(creditId);
+    isLoading = false;
+    notifyListeners();
+  }
+  void getCreditById(String creditId)async{
+    isLoading = true;
+    _creditSelected = await CreditService().getCreditbyId(creditId);
+    isLoading = false;
+    notifyListeners();
+
+  }
+
+
 }
