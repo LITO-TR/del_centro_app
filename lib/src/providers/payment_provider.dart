a8306dfde7c227706810e1d3ec5c1e31b9d0ecda
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-02-17 11:00:24 -0500

    payment provider added

--- lib/src/providers/payment_provider.dart
+++ lib/src/providers/payment_provider.dart
@@ -1,5 +1,12 @@
+import 'package:del_centro_app/src/core/services/payment_service.dart';
 import 'package:flutter/cupertino.dart';
 
 class PaymentProvider extends ChangeNotifier{
+  bool isLoading = false;
+  void makePayment(String paymentId, String method, double customerPayment)async{
+    isLoading = true;
+    await PaymentService().setPayment(paymentId, method, customerPayment);
+    notifyListeners();
+  }
 
 }
