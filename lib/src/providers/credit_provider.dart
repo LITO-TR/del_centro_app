0b0dd8fbb46f580333a89e7bee326851e9265773
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-02-18 01:08:23 -0500

    get credits by creation date for credit provider

--- lib/src/providers/credit_provider.dart
+++ lib/src/providers/credit_provider.dart
@@ -8,6 +8,9 @@ import '../core/services/credit_service.dart';
 class CreditProvider extends ChangeNotifier{
   Credit _creditCreated = Credit();
   bool isLoading = false;
+  List<Credit> _listCreditsByCreationDate = [];
+
+  List<Credit> get listCreditsByCreationDate => _listCreditsByCreationDate;
 
   Credit get creditCreated => _creditCreated;
 
@@ -21,6 +24,13 @@ class CreditProvider extends ChangeNotifier{
     isLoading = false;
     _creditCreated = await CreditService().createCredit(creditAmount, decimalInterest, numberOfPayments, paymentMethod, mora, customer);
     isLoading = true;
+    notifyListeners();
+  }
+
+  void getCreditsByCreationDate(String day, String month, String year)async{
+    isLoading = true;
+    _listCreditsByCreationDate = await CreditService().getCreditsByCreationDate(day, month, year);
+    isLoading = false;
     notifyListeners();
   }
 }
