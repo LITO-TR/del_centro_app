0ce7c6e1a827de420463b6087df6239afd8c4d43
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2023-02-18 01:11:24 -0500

    list payments by date added for payment provider

--- lib/src/providers/payment_provider.dart
+++ lib/src/providers/payment_provider.dart
@@ -1,12 +1,33 @@
 import 'package:del_centro_app/src/core/services/payment_service.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../core/models/payment.dart';
+
 class PaymentProvider extends ChangeNotifier{
+  List<Payment> _listPaymentsByDate = [];
+
+  List<Payment> get listPaymentsByDate => _listPaymentsByDate;
   bool isLoading = false;
-  void makePayment(String paymentId, String method, double customerPayment)async{
+
+
+
+  void getPaymentsByDate(String day, String month, String year)async{
     isLoading = true;
-    await PaymentService().setPayment(paymentId, method, customerPayment);
+    _listPaymentsByDate = await PaymentService().getPaymentsByDate(day, month, year);
+    isLoading = false;
+    notifyListeners();
+
+  }
+
+  void makePaymentDaily(int index,String paymentId, String method, double customerPayment)async{
+    isLoading = true;
+    _listPaymentsByDate[index] = await PaymentService().setPayment(paymentId, method, customerPayment);
+    isLoading = false;
     notifyListeners();
   }
+
+
+
+
 
 }
